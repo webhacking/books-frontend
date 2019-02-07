437da43e9b15f0c6160fdd0995726e11e335c822
JeongSik Yun <jeongsik@ridi.com>
2019-02-07 23:37:19 +0900

    @ridi/web-ui test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,30 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
 import * as React from 'react';
 import styled from '@emotion/styled';
+
+// @ts-ignore
+import { Book } from '@ridi/web-ui/dist/index.node';
+import { css } from '@emotion/core';
 
 // emotion test
 const Div = styled.div`
   color: red;
 `;
 
-export default () => <Div>Home</Div>;
+export default class Home extends React.Component {
+  public render() {
+    return (
+      <div>
+        <Div>Home Test</Div>
+        <Book.Thumbnail
+          thumbnailWidth={240}
+          css={css`
+            border: 4px dashed blueviolet;
+          `}
+          thumbnailUrl={'https://misc.ridibooks.com/cover/1242000770/xxlarge'}
+        />
+      </div>
+    );
+  }
+}
