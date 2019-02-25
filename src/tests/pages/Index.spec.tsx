d77ba8c5d750dbf14855f050460ea2e86db9865f
Jeong-Sik Yun <2962937-yousleepwhen@users.noreply.gitlab.com>
2019-02-25 07:11:14 +0000

    CloudFront 설정 추가, Workbox 기본 설정 추가,

--- src/tests/pages/Index.spec.tsx
+++ src/tests/pages/Index.spec.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import About from '../index';
+import Index from 'src/pages';
 import { render, cleanup } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 afterEach(cleanup);
 
 test('should be render Index Component', () => {
-  const { getByText } = render(<About id={'12345'} />);
+  const { getByText } = render(<Index />);
 
   expect(getByText(/Home/)).toHaveTextContent('Home');
 });
---
+++ static/service-worker.js
@@ -0,0 +1,11 @@
+if ('workbox' in self) {
+  workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+  workbox.loadModule('workbox-strategies');
+
+  workbox.skipWaiting();
+  workbox.clientsClaim();
+
+  // ridibooks 요청일 경우 stateWhileRevalidate 캐싱
+  workbox.routing.registerRoute(/.*(?:ridibooks)\.com.*$/, workbox.strategies.staleWhileRevalidate());
+}
