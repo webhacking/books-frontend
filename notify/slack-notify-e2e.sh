2e3bec045fb17787177050cefc972c79ad7aafc2
JeongSik Yun <jeongsik@ridi.com>
2019-04-12 16:41:32 +0900

    Test CI

---
+++ notify/slack-notify-e2e.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+STATUS="$(cat .job_status)"
+STAGE="$(cat .stage)"
+ENV=${ENVIRONMENT:="DEVELOPMENT"}
+BRANCH_NAME=${CI_COMMIT_REF_NAME:="NONE"}
+
+[[ $STATUS = "FAIL" ]] && COLOR="#D93826" || COLOR="#5ABF0D"
+
+
+if [ ${ENV} = "DEVELOPMENT" ]
+then
+  if [ ${STAGE} != "E2E" ]
+  then
+   curl -X POST \
+   -H 'Content-type: application/json' \
+   --data '{
+     "attachments": [{
+     "title": "'"books.ridi.io"'",
+     "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
+     "color": "#D93826",
+     "fields": [
+        { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
+        { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
+        { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
+        { "title": "JOB Result", "value": "FAIL", "short": true },
+      ]
+    }]
+  }'\
+  ${DEV_SLACK_WEB_HOOK}
+  elif [ ${STAGE} = "E2E" ]
+  then
+   curl -X POST \
+   -H 'Content-type: application/json' \
+   --data '{
+     "attachments": [{
+     "title": "'"books.ridi.io"'",
+     "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
+     "color": "'"${COLOR}"'",
+     "fields": [
+        { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
+        { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
+        { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
+        { "title": "JOB Result", "value": "'"${STATUS}"'", "short": true },
+        { "title": "E2E Video", "value": "'"https://gitlab.com/ridicorp/frontend/books/-/jobs/artifacts/${BRANCH_NAME}/download?job=e2e-${BRANCH_NAME}"'", "short": true}
+      ]
+    }]
+  }'\
+  ${DEV_SLACK_WEB_HOOK}
+  fi
+fi
--- notify/slack-notify.sh
+++ notify/slack-notify.sh
@@ -10,42 +10,20 @@ BRANCH_NAME=${CI_COMMIT_REF_NAME:="NONE"}
 
 if [ ${ENV} = "DEVELOPMENT" ]
 then
-  if [ ${STAGE} != "E2E" ]
-  then
-   curl -X POST \
-   -H 'Content-type: application/json' \
-   --data '{
-     "attachments": [{
-     "title": "'"books.ridi.io"'",
-     "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
-     "color": "'"${COLOR}"'",
-     "fields": [
-        { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
-        { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
-        { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
-        { "title": "JOB Result", "value": "'"${STATUS}"'", "short": true },
-      ]
-    }]
+  curl -X POST \
+  -H 'Content-type: application/json' \
+  --data '{
+    "attachments": [{
+    "title": "'"books.ridi.io"'",
+    "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
+    "color": "'"${COLOR}"'",
+    "fields": [
+       { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
+       { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
+       { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
+       { "title": "JOB Result", "value": "'"${STATUS}"'", "short": true },
+     ]
+   }]
   }'\
-  ${DEV_SLACK_WEB_HOOK}
-  elif [ ${STAGE} = "E2E" ]
-  then
-   curl -X POST \
-   -H 'Content-type: application/json' \
-   --data '{
-     "attachments": [{
-     "title": "'"books.ridi.io"'",
-     "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
-     "color": "'"${COLOR}"'",
-     "fields": [
-        { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
-        { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
-        { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
-        { "title": "JOB Result", "value": "'"${STATUS}"'", "short": true },
-        { "title": "E2E Video", "value": "'"https://gitlab.com/ridicorp/frontend/books/-/jobs/artifacts/${BRANCH_NAME}/download?job=e2e-${BRANCH_NAME}"'", "short": true}
-      ]
-    }]
-  }'\
-  ${DEV_SLACK_WEB_HOOK}
-  fi
+${DEV_SLACK_WEB_HOOK}
 fi
