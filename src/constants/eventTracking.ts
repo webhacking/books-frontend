54dd18a33ea88586f449907eb2ef5dffdbd3a3ed
JeongSik Yun <jeongsik@ridi.com>
2019-08-29 21:57:51 +0900

    Add event-tracker package

---
+++ src/constants/eventTracking.ts
@@ -0,0 +1,3 @@
+export const GA_KEY = 'UA-10567409-2';
+export const FB_KEYS = ['826424444060066', '369643527137452'];
+export const GTM_KEY = 'GTM-TVKHPCC';
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,7 @@ import 'slick-carousel/slick/slick.css';
 import { PartialSeparator } from 'src/components/Misc';
 import { cache } from 'emotion';
 import createCache from '@emotion/cache';
+// import { Tracker, DeviceType } from '@ridi/event-tracker';
 
 interface StoreAppProps {
   store: Store<StoreRootState>;
