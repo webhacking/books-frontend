809fdb87fcf8e1714b7d420cef826a0c2f583738
forsyphilis <forsyphilis@gmail.com>
2019-11-24 22:03:17 +0900

    메타 정보 추가

--- src/pages/Meta.tsx
+++ src/pages/Meta.tsx
@@ -1,12 +1,24 @@
 export default () => (
   <>
-    <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
     <meta charSet="UTF-8" />
+    <meta name="twitter:site" content="@ridibooks" />
     <meta
       name="viewport"
       content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=0"
     />
     <meta name="theme-color" content="#339CF2" />
+    <meta name="keywords" content="리디북스,리디,ridibooks,ridi,책,전자책,ebook" />
     <meta name="format-detection" content="telephone=no" />
+    <meta property="og:url" content="https://ridibooks.com" />
+    <meta property="og:title" content="RIDIBOOKS" />
+    <meta
+      property="og:description"
+      content="최고의 eBook 서비스, 리디북스! 227만 권의 eBook, 특별반값 도서, 최신 베스트셀러에서 빌려보는 만화/판무/잡지, 내 문서파일 (PDF/TXT/ePub) 뷰어 기능까지!"
+    />
+    <meta
+      name="description"
+      content="최고의 eBook 서비스, 리디북스! 227만 권의 eBook, 특별반값 도서, 최신 베스트셀러에서 빌려보는 만화/판무/잡지, 내 문서파일 (PDF/TXT/ePub) 뷰어 기능까지!"
+    />
   </>
 );
