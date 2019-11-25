14e90beaa66fb7405fe894a3bc1fed0208c8913c
forsyphilis <forsyphilis@gmail.com>
2019-11-26 05:01:12 +0900

    Tracker 옵션 추가

--- src/constants/eventTracking.ts
+++ src/constants/eventTracking.ts
@@ -1,3 +1,4 @@
 export const GA_KEY = 'UA-10567409-2';
-export const FB_KEYS = ['826424444060066', '369643527137452'];
+export const FB_KEYS = '826424444060066';
+// export const FB_KEYS = ['826424444060066', '369643527137452'];
 export const GTM_KEY = 'GTM-TVKHPCC';
--- src/hooks/useEveneTracker.tsx
+++ src/hooks/useEveneTracker.tsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from 'react';
 import { DeviceType, Tracker } from '@ridi/event-tracker';
-import { GA_KEY, GTM_KEY } from 'src/constants/eventTracking';
+import { FB_KEYS, GA_KEY, GTM_KEY } from 'src/constants/eventTracking';
 
 const initTracker = (userId: string) => {
   if (typeof window !== 'undefined') {
     const tracker = new Tracker({
       userId: userId || null,
-      // Todo device 판단
-      deviceType: DeviceType.PC || null,
+      // Todo device 판단  User-Agent ?
+      deviceType: window.innerWidth > 999 ? DeviceType.PC : DeviceType.Mobile, // user agent 판단? 아니면?
       beaconOptions: {
-        use: true,
+        use: true, // Todo CSP Check
       },
       // eslint-disable-next-line no-process-env
       debug: process.env.NODE_ENV !== 'production',
       gaOptions: {
         trackingId: GA_KEY,
+        pathPrefix: window.location.pathname,
+        fields: {
+          campaignName: 'RENEWAL',
+        },
+      },
+      pixelOptions: {
+        pixelId: FB_KEYS, // Multiple Keys?
       },
       tagManagerOptions: {
         trackingId: GTM_KEY,
