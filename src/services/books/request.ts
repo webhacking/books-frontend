21da26e97b1f98ed22e2a37948430235fa8a7d39
forsyphilis <forsyphilis@gmail.com>
2019-11-28 13:38:04 +0900

    catch 문 추가

--- src/services/books/request.ts
+++ src/services/books/request.ts
@@ -1,15 +1,23 @@
 import getConfig from 'next/config';
 import * as BookApi from 'src/types/book';
 import axios from 'src/utils/axios';
+import sentry from 'src/utils/sentry';
+const { captureException } = sentry();
+
 const { publicRuntimeConfig } = getConfig();
 export const requestBooks = async (b_ids: string[]) => {
-  const { data } = await axios.get<BookApi.Book[]>('/books', {
-    baseURL: publicRuntimeConfig.BOOK_API,
-    params: {
-      b_ids: b_ids.join(),
-    },
-  });
-  return data;
+  try {
+    const { data } = await axios.get<BookApi.Book[]>('/books', {
+      baseURL: publicRuntimeConfig.BOOK_API,
+      params: {
+        b_ids: b_ids.join(),
+      },
+    });
+    return data;
+  } catch (error) {
+    captureException(error);
+    return [];
+  }
 };
 
 export const checkAvailableAtRidiSelect = async (b_ids: string[]) => {
