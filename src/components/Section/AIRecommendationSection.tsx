ddf7ec8ccefddd4f8d382425f29df3a21dd9f2a6
JeongSik Yun <jeongsik@ridi.com>
2019-12-05 14:45:23 +0900

    ai 추천영역 방어코드 추가

--- src/components/Section/AIRecommendationSection.tsx
+++ src/components/Section/AIRecommendationSection.tsx
@@ -50,8 +50,9 @@ const AiRecommendationSection: React.FC<AiRecommendationSectionProps> = props =>
     if (!items && loggedUser) {
       requestAiRecommendationItems();
     }
+    console.log(aiItems);
   }, [dispatch, items, genre, loggedUser]);
-  if (!loggedUser || aiItems.length < 1) {
+  if (!loggedUser || !aiItems || aiItems.length < 1) {
     return null;
   }
   return (
