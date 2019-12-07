57402b1956399167b9fda8a09d7e0fd31351f0e3
JeongSik Yun <jeongsik@ridi.com>
2019-12-07 17:24:43 +0900

    cart 숫자에 background-clip important 추가

--- src/components/Tabs/MainTab.tsx
+++ src/components/Tabs/MainTab.tsx
@@ -375,7 +375,7 @@ export const MainTab: React.FC<MainTabProps> = props => {
                 `}>
                 <div
                   css={css`
-                    background-clip: padding-box;
+                    background-clip: padding-box !important;
                     align-items: center;
                     border-radius: 6px;
                     border: 2px solid #1f8ce6;
