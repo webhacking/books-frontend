4840883a7a347be41a3630ee0c6d65b5628f0b2b
JeongSik Yun <jeongsik@ridi.com>
2019-12-11 15:56:26 +0900

    ai 추천영역에 항상 빈 배열을 넣는 부분 수정

--- src/components/Section/AIRecommendationSection.tsx
+++ src/components/Section/AIRecommendationSection.tsx
@@ -59,7 +59,7 @@ const AiRecommendationSection: React.FC<AiRecommendationSectionProps> = props =>
   }
   return (
     <SelectionBook
-      items={[]}
+      items={aiItems || []}
       slug={`${slug}-ai-rcmd`}
       title={loggedUser ? `${loggedUser.id} ${title}` : 'AI 추천'}
       extra={extra}
