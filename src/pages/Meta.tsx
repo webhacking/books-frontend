0e1c370cb74ac61730eebe7df08b66e3e49213a9
JeongSik Yun <jeongsik@ridi.com>
2019-12-16 11:49:20 +0900

    og image 추가

--- src/pages/Meta.tsx
+++ src/pages/Meta.tsx
@@ -1,3 +1,6 @@
+import getConfig from 'next/config';
+const { publicRuntimeConfig } = getConfig();
+
 export default () => (
   <>
     <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
@@ -7,10 +10,17 @@ export default () => (
       name="viewport"
       content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=0"
     />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@ridibooks" />
+    <meta property="fb:app_id" content="208688205808637" />
     <meta name="theme-color" content="#339CF2" />
     <meta name="keywords" content="리디북스,리디,ridibooks,ridi,책,전자책,ebook" />
     <meta name="format-detection" content="telephone=no" />
     <meta property="og:url" content="https://ridibooks.com" />
+    <meta
+      property="og:image"
+      content={`${publicRuntimeConfig.STATIC_CDN_URL}/static/images/ridibooks.png`}
+    />
     <meta property="og:title" content="RIDIBOOKS" />
     <meta
       property="og:description"
