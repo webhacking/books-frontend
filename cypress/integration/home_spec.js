6fca4c6460c1e98d70f9a2a6967270a70fe47a4c
JeongSik Yun <jeongsik@ridi.com>
2019-12-30 15:55:03 +0900

    주석 처리 E2E 원복, video 녹화 옵션 원복

--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -5,38 +5,40 @@ describe('Genre Home Test', function() {
     cy.visit('/');
     cy.contains('일반');
     cy.contains('로맨스');
-    // cy.viewport(700, 500);
+    cy.viewport(700, 500);
+    cy.contains('일반');
+    cy.contains('로맨스');
   });
 
-  // it('인스턴트 검색이 존재', () => {
-  //   cy.get('input')
-  //     .should('have.attr', 'placeholder', labels.searchPlaceHolder)
-  //     .click();
-  //
-  //   cy.viewport(700, 500);
-  //   cy.get('input')
-  //     .should('have.attr', 'placeholder', labels.searchPlaceHolder)
-  //     .click();
-  // });
-  //
-  // it('검색 히스토리 목록 표시', () => {
-  //   cy.clearLocalStorage();
-  //   cy.get('input')
-  //     .click()
-  //     .type('{enter}');
-  //   cy.viewport(700, 500);
-  //   cy.wait(250);
-  //   cy.get('input')
-  //     .should('have.attr', 'placeholder', labels.searchPlaceHolder)
-  //     .click()
-  //     .type('test{enter}');
-  //   cy.wait(250);
-  //   cy.get('input').clear();
-  //   cy.contains(labels.turnOffSearchHistory);
-  //   cy.contains(labels.clearSearchHistory);
-  //   cy.contains(labels.turnOffSearchHistory).click();
-  //   cy.contains(labels.turnOffStatus);
-  //   cy.contains(labels.turnOnSearchHistory).click();
-  //   cy.contains(labels.clearSearchHistory).click();
-  // });
+  it('인스턴트 검색이 존재', () => {
+    cy.get('input')
+      .should('have.attr', 'placeholder', labels.searchPlaceHolder)
+      .click();
+
+    cy.viewport(700, 500);
+    cy.get('input')
+      .should('have.attr', 'placeholder', labels.searchPlaceHolder)
+      .click();
+  });
+
+  it('검색 히스토리 목록 표시', () => {
+    cy.clearLocalStorage();
+    cy.get('input')
+      .click()
+      .type('{enter}');
+    cy.viewport(700, 500);
+    cy.wait(250);
+    cy.get('input')
+      .should('have.attr', 'placeholder', labels.searchPlaceHolder)
+      .click()
+      .type('test{enter}');
+    cy.wait(250);
+    cy.get('input').clear();
+    cy.contains(labels.turnOffSearchHistory);
+    cy.contains(labels.clearSearchHistory);
+    cy.contains(labels.turnOffSearchHistory).click();
+    cy.contains(labels.turnOffStatus);
+    cy.contains(labels.turnOnSearchHistory).click();
+    cy.contains(labels.clearSearchHistory).click();
+  });
 });
