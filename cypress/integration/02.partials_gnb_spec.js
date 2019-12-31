a63b7ce181f4d89b88a14fa047ad194e40566005
JeongSik Yun <jeongsik@ridi.com>
2019-12-31 13:13:32 +0900

    장르별로 방문 테스트 추가

--- cypress/integration/02.partials_gnb_spec.js
+++ cypress/integration/02.partials_gnb_spec.js
@@ -4,4 +4,10 @@ describe('Partials GNB Test', function() {
     cy.contains('로그인');
     // expect(true).to.equal(true);
   });
+
+  it('GNB 로그인 쿼리가 있을 경우', () => {
+    cy.visit('/partials/gnb?is_login=true');
+    cy.contains('내 서재');
+    cy.contains('캐시 충전');
+  });
 });
--- cypress/integration/home_spec.js
+++ cypress/integration/home_spec.js
@@ -1,6 +1,6 @@
 import labels from '../../src/labels/instantSearch.json';
 
-describe('Genre Home Test', function() {
+describe('장르 홈 방문 테스트', function() {
   it('홈 방문이 가능', function() {
     cy.visit('/');
     cy.contains('일반');
@@ -8,17 +8,48 @@ describe('Genre Home Test', function() {
     cy.viewport(700, 500);
     cy.contains('일반');
     cy.contains('로맨스');
+    cy.contains('판타지');
+    cy.contains('BL');
+    cy.contains('만화');
   });
 
-  it('인스턴트 검색이 존재', () => {
-    cy.get('input')
-      .should('have.attr', 'placeholder', labels.searchPlaceHolder)
-      .click({ multiple: true, force: true });
+  it('로맨스 방문 가능', function() {
+    cy.visit('/romance/');
+    cy.contains('단행본');
+    cy.contains('연재');
+  });
 
-    cy.viewport(700, 500);
-    cy.get('input')
-      .should('have.attr', 'placeholder', labels.searchPlaceHolder)
-      .click({ multiple: true, force: true });
+  it('로맨스 연재 방문 가능', function() {
+    cy.visit('/romance-serial/');
+    cy.contains('단행본');
+    cy.contains('연재');
+  });
+
+  it('판타지 방문 가능', function() {
+    cy.visit('/fantasy/');
+    cy.contains('단행본');
+    cy.contains('연재');
+  });
+  it('판타지 연재 방문 가능', function() {
+    cy.visit('/fantasy-serial/');
+    cy.contains('단행본');
+    cy.contains('연재');
+  });
+  it('로맨스 방문 가능', function() {
+    cy.visit('/romance');
+    cy.contains('단행본');
+    cy.contains('연재');
+  });
+
+  it('BL 방문 가능', function() {
+    cy.visit('/bl/');
+    cy.contains('단행본');
+    cy.contains('연재');
+  });
+  it('BL 연재 방문 가능', function() {
+    cy.visit('/bl-serial/');
+    cy.contains('단행본');
+    cy.contains('연재');
   });
 
   // it('검색 히스토리 목록 표시', () => {
@@ -41,4 +72,17 @@ describe('Genre Home Test', function() {
   //   cy.contains(labels.turnOnSearchHistory).click({ multiple: true, force: true });
   //   cy.contains(labels.clearSearchHistory).click({ multiple: true, force: true });
   // });
+});
+
+describe('홈 기능 테스트', () => {
+  it('인스턴트 검색이 존재', () => {
+    cy.get('input')
+      .should('have.attr', 'placeholder', labels.searchPlaceHolder)
+      .click({ multiple: true, force: true });
+
+    cy.viewport(700, 500);
+    cy.get('input')
+      .should('have.attr', 'placeholder', labels.searchPlaceHolder)
+      .click({ multiple: true, force: true });
+  });
 });
