5cf9ff26a361a3a0bcefe53b63d0519e7575d04f
Heejae Jang <j132134@gmail.com>
2020-01-15 15:49:15 +0900

    CSR 시, fetchHomeSections를 컴포넌트 내에서 처리

--- src/pages/[genre]/index.tsx
+++ src/pages/[genre]/index.tsx
@@ -1,12 +1,13 @@
-import React, { useCallback, useEffect } from 'react';
+import React, {
+  useState, useCallback, useEffect, useRef,
+} from 'react';
 import Head from 'next/head';
 import { ConnectedInitializeProps } from 'src/types/common';
 import { GenreTab } from 'src/components/Tabs';
 import cookieKeys, { DEFAULT_COOKIE_EXPIRES } from 'src/constants/cookies';
-import Router from 'next/router';
 import * as Cookies from 'js-cookie';
 import titleGenerator from 'src/utils/titleGenerator';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { Page, Section } from 'src/types/sections';
 import { HomeSectionRenderer } from 'src/components/Section/HomeSectionRenderer';
@@ -15,13 +16,11 @@ import { keyToArray } from 'src/utils/common';
 import axios from 'src/utils/axios';
 import { booksActions } from 'src/services/books';
 import { Request } from 'express';
-import { ServerResponse } from 'http';
 import sentry from 'src/utils/sentry';
 import { categoryActions } from 'src/services/category';
 import { NextPage } from 'next';
 import { useEventTracker } from 'src/hooks/useEventTracker';
 import { RootState } from 'src/store/config';
-import useIsSelectFetch from 'src/hooks/useIsSelectFetch';
 import { css } from '@emotion/core';
 
 import { DeviceType } from 'src/components/Context/DeviceType';
@@ -74,12 +73,47 @@ const setCookie = (genre: string) => {
   });
 };
 
+const usePrevious = <T extends {}>(value: T) => {
+  const ref = useRef<T>();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
 export const Home: NextPage<HomeProps> = (props) => {
   const { loggedUser } = useSelector((state: RootState) => state.account);
-  const bIds = keyToArray(
-    props.branches.filter((section) => section.extra.use_select_api),
-    'b_id',
-  );
+  const dispatch = useDispatch();
+
+  const [branches, setBranches] = useState(props.branches);
+  const previousGenre = usePrevious(props.genre);
+  useEffect(() => {
+    const fetch = async () => {
+      if (!branches || previousGenre !== props.genre) {
+        // store.dispatch({ type: booksActions.setFetching.type, payload: true });
+        setBranches([]);
+        const result = await fetchHomeSections(
+          // @ts-ignore
+          props.genre || 'general',
+        );
+        setBranches(result.branches);
+        const bIds = keyToArray(result.branches, 'b_id');
+        dispatch({ type: booksActions.insertBookIds.type, payload: bIds });
+        const categoryIds = keyToArray(result.branches, 'category_id');
+        dispatch({
+          type: categoryActions.insertCategoryIds.type,
+          payload: categoryIds,
+        });
+        const selectBIds = keyToArray(
+          result.branches.filter((section) => section.extra.use_select_api),
+          'b_id',
+        );
+        dispatch({ type: booksActions.checkSelectBook.type, payload: selectBIds });
+      }
+    };
+    fetch();
+  }, [props.genre, dispatch]);
+
   const [tracker] = useEventTracker();
 
   const setPageView = useCallback(() => {
@@ -92,11 +126,11 @@ export const Home: NextPage<HomeProps> = (props) => {
     }
   }, [tracker]);
 
-  useIsSelectFetch(bIds);
   useEffect(() => {
     setCookie(props.genre);
     setPageView();
-  }, [props.genre, loggedUser, props.branches, setPageView]);
+  }, [props.genre, loggedUser, setPageView]);
+
   const { genre } = props;
   const currentGenre = genre || 'general';
   return (
@@ -106,12 +140,11 @@ export const Home: NextPage<HomeProps> = (props) => {
       </Head>
       <GenreTab currentGenre={currentGenre} />
       <DeviceType>
-        {props.branches
-          && props.branches.map((section, index) => (
-            <React.Fragment key={index}>
-              <HomeSectionRenderer section={section} order={index} genre={currentGenre} />
-            </React.Fragment>
-          ))}
+        {branches && branches.map((section, index) => (
+          <React.Fragment key={index}>
+            <HomeSectionRenderer section={section} order={index} genre={currentGenre} />
+          </React.Fragment>
+        ))}
         <div
           css={css`
             margin-bottom: 24px;
@@ -124,7 +157,11 @@ export const Home: NextPage<HomeProps> = (props) => {
 
 Home.getInitialProps = async (ctx: ConnectedInitializeProps) => {
   const {
-    query, res, req, store,
+    query,
+    res,
+    req,
+    store,
+    isServer,
   } = ctx;
 
   const { genre = 'general' } = query;
@@ -132,7 +169,7 @@ Home.getInitialProps = async (ctx: ConnectedInitializeProps) => {
     throw new Error('Not Found');
   }
 
-  if (req && res) {
+  if (isServer) {
     if (res.statusCode !== 302) {
       try {
         // store.dispatch({ type: booksActions.setFetching.type, payload: true });
@@ -155,44 +192,9 @@ Home.getInitialProps = async (ctx: ConnectedInitializeProps) => {
           ...result,
         };
       } catch (error) {
-        console.log(error);
         captureException(error, ctx);
-        // redirect(req, res, '/error');
+        throw new Error(error);
       }
-    }
-  } else {
-    // Client Side
-    try {
-      const result = await fetchHomeSections(
-        // @ts-ignore
-        genre || 'general',
-        null,
-        // Hack
-        // 사파리 BFCache 디스크에서 캐시 된 데이터가 그대로 사용되는데 끄걸 회피하기 위해 Query Param 에 초를 삽입하는 꼼수
-        // 30초 단위로 잘라 보냄
-        { sec: Math.floor(Date.now() / 30000) },
-      );
-      const bIds = keyToArray(result.branches, 'b_id');
-      store.dispatch({ type: booksActions.insertBookIds.type, payload: bIds });
-      const categoryIds = keyToArray(result.branches, 'category_id');
-      store.dispatch({
-        type: categoryActions.insertCategoryIds.type,
-        payload: categoryIds,
-      });
-      const selectBIds = keyToArray(
-        result.branches.filter((section) => section.extra.use_select_api),
-        'b_id',
-      );
-      store.dispatch({ type: booksActions.checkSelectBook.type, payload: selectBIds });
-      return {
-        genre,
-        store,
-        ...query,
-        ...result,
-      };
-    } catch (error) {
-      captureException(error, ctx);
-      Router.push('/error');
     }
   }
   return {
