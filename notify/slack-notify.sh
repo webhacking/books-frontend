973b5fd8735419ac3723339c44bdf86fbb72336f
Wonwoo Choi <chwo9843@gmail.com>
2020-01-16 08:38:41 +0000

    Cleaner slack notify

--- notify/slack-notify.sh
+++ notify/slack-notify.sh
@@ -4,6 +4,7 @@ STATUS="$(cat .job_status)"
 STAGE="$(cat .stage)"
 ENV=${ENVIRONMENT:="development"}
 BRANCH_NAME=${CI_COMMIT_REF_NAME:="NONE"}
+COMMIT_TITLE=${CI_COMMIT_TITLE:="NONE"}
 
 [[ $STATUS = "FAIL" ]] && COLOR="#D93826" || COLOR="#5ABF0D"
 
@@ -11,64 +12,62 @@ BRANCH_NAME=${CI_COMMIT_REF_NAME:="NONE"}
 if [ $STAGE = "TEST" ] && [ $STATUS = "FAIL" ]
 then
   curl -X POST \
-  -H 'Content-type: application/json' \
-  --data '{
-    "attachments": [{
-    "title": "'"books.ridi.io"'",
-    "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
+    -H 'Content-type: application/json' -d @- ${DEV_SLACK_WEB_HOOK} <<EOF
+{
+  "attachments": [{
+    "title": "books.ridi.io",
+    "text": "${COMMIT_TITLE}",
     "color": "#D93826",
     "fields": [
-       { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
-       { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
-       { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
-       { "title": "Job Result", "value": "'"${STATUS}"'", "short": true },
-     ]
-   }]
-  }'\
-  ${DEV_SLACK_WEB_HOOK}
+      { "title": "Branch", "value": "${BRANCH_NAME}", "short": true },
+      { "title": "Revision", "value": "${CI_COMMIT_SHA:="NONE"}", "short": true },
+      { "title": "CI Stage", "value": "${STAGE}", "short": true },
+      { "title": "Job Result", "value": "${STATUS}", "short": true }
+    ]
+  }]
+}
+EOF
 elif [ ${ENV} = "development" ] && [ $STAGE != "TEST" ]
 then
   curl -X POST \
-  -H 'Content-type: application/json' \
-  --data '{
-    "attachments": [{
-    "title": "'"books.ridi.io"'",
-    "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
-    "color": "'"${COLOR}"'",
+    -H 'Content-type: application/json' -d @- ${DEV_SLACK_WEB_HOOK} <<EOF
+{
+  "attachments": [{
+    "title": "books.ridi.io",
+    "text": "${COMMIT_TITLE}",
+    "color": "${COLOR}",
     "fields": [
-       { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
-       { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
-       { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
-       { "title": "JOB Result", "value": "'"${STATUS}"'", "short": true },
-     ]
-   }]
-  }'\
-  ${DEV_SLACK_WEB_HOOK}
+      { "title": "Branch", "value": "${BRANCH_NAME}", "short": true },
+      { "title": "Revision", "value": "${CI_COMMIT_SHA:="NONE"}", "short": true },
+      { "title": "CI Stage", "value": "${STAGE}", "short": true },
+      { "title": "JOB Result", "value": "${STATUS}", "short": true }
+    ]
+  }]
+}
+EOF
 elif [ ${ENV} = "production" ]
 then
   curl -X POST \
-  -H 'Content-type: application/json' \
-  --data '{
-    "attachments": [{
-    "title": "'"books.ridibooks.com"'",
-    "text": "'"${CI_COMMIT_TITLE:="NONE"}"'",
-    "color": "'"${COLOR}"'",
+    -H 'Content-type: application/json' -d @- ${PROD_SLACK_WEB_HOOK} <<EOF
+{
+  "attachments": [{
+    "title": "books.ridibooks.com",
+    "text": "${COMMIT_TITLE}",
+    "color": "${COLOR}",
     "fields": [
-       { "title": "Branch", "value": "'"${BRANCH_NAME}"'", "short": true },
-       { "title": "Revision", "value": "'"${CI_COMMIT_SHA:="NONE"}"'", "short": true },
-       { "title": "CI Stage", "value": "'"${STAGE}"'", "short": true },
-       { "title": "JOB Result", "value": "'"${STATUS}"'", "short": true },
-
-     ],
-   }],
-
-   "blocks": [{
-         "type": "section",
-         "text": {
-           "type": "mrkdwn",
-           "text": "\n :book: \n ```'"${HISTORY}"'``` "
-         }
-   }],
-  }'\
-  ${PROD_SLACK_WEB_HOOK}
+       { "title": "Branch", "value": "${BRANCH_NAME}", "short": true },
+       { "title": "Revision", "value": "${CI_COMMIT_SHA:="NONE"}", "short": true },
+       { "title": "CI Stage", "value": "${STAGE}", "short": true },
+       { "title": "JOB Result", "value": "${STATUS}", "short": true }
+    ],
+  }],
+  "blocks": [{
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": ":book:\n${HISTORY}"
+    }
+  }]
+}
+EOF
 fi
