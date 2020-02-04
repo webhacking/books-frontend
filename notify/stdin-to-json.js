a50ed58d4aaa8b78e2e0a39f3c1a359c1c433eff
Jeong-Sik Yun <2962937-yousleepwhen@users.noreply.gitlab.com>
2020-02-04 04:58:29 +0000

    Apply suggestion to notify/stdin-to-json.js

--- notify/stdin-to-json.js
+++ notify/stdin-to-json.js
@@ -2,6 +2,6 @@ const buffers = [];
 process.stdin.on('data', data => buffers.push(data));
 process.stdin.on('end', () => {
   const data = Buffer.concat(buffers);
-  console.log(`:book: 변경 사항\\n${String(data)}`.replace(/\*/g, '\\n'));
+  console.log(JSON.stringify(`:book: 변경 사항\n${String(data)}`));
 });
 process.stdin.on('error', console.error);
