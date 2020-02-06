95c5c3ce4ab79eac45ba9f8e755ac622b54b71a7
Jeong-Sik Yun <2962937-yousleepwhen@users.noreply.gitlab.com>
2020-02-06 07:42:22 +0000

    반복되는 inline svg 를 img 태그 사용

--- src/components/Badge/AdultBadge.tsx
+++ src/components/Badge/AdultBadge.tsx
@@ -2,17 +2,15 @@ import { ADULT_BADGE_URL } from 'src/constants/icons';
 import { css } from '@emotion/core';
 import React from 'react';
 
+const badgeStyle = css`
+  display: block;
+  position: absolute;
+  right: 3px;
+  top: 3px;
+  width: 20px !important;
+  height: 20px !important;
+`;
+
 export const AdultBadge: React.FC = () => (
-  <img
-    src={ADULT_BADGE_URL}
-    alt={'성인 전용 도서'}
-    css={css`
-      display: block;
-      position: absolute;
-      right: 3px;
-      top: 3px;
-      width: 20px !important;
-      height: 20px !important;
-    `}
-  />
+  <img src={ADULT_BADGE_URL} alt={'성인 전용 도서'} css={badgeStyle} />
 );
