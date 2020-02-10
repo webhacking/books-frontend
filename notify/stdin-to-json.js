acf86ea3a0c85b3c43d59121b04e4cfc32655d08
Heejae Jang <j132134@gmail.com>
2020-02-10 14:04:34 +0900

    Remove unused files

--- notify/stdin-to-json.js
+++
@@ -1,7 +0,0 @@
-const buffers = [];
-process.stdin.on('data', data => buffers.push(data));
-process.stdin.on('end', () => {
-  const data = Buffer.concat(buffers);
-  console.log(JSON.stringify(`:book: 변경 사항\n${String(data)}`));
-});
-process.stdin.on('error', console.error);
