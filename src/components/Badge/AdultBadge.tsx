60b73d2d04a60cd82c66c2c53f6a7b18455174ad
JeongSik Yun <jeongsik@ridi.com>
2020-02-17 10:44:01 +0900

    AdultBadge styled-component 로 변경

--- src/components/Badge/AdultBadge.tsx
+++ src/components/Badge/AdultBadge.tsx
@@ -1,8 +1,8 @@
 import { ADULT_BADGE_URL } from 'src/constants/icons';
-import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 import React from 'react';
 
-const badgeStyle = css`
+const AdultBadgeImg = styled.img`
   display: block;
   position: absolute;
   right: 3px;
@@ -12,5 +12,5 @@ const badgeStyle = css`
 `;
 
 export const AdultBadge: React.FC = () => (
-  <img src={ADULT_BADGE_URL} alt="성인 전용 도서" css={badgeStyle} />
+  <AdultBadgeImg src={ADULT_BADGE_URL} alt="성인 전용 도서" />
 );
