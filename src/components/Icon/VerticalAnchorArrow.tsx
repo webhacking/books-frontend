856b31ad52272bff9c664dcedaac1f1145657da8
JeongSik Yun <jeongsik@ridi.com>
2020-02-17 17:04:28 +0900

    타이틀에 반복되는 오른쪽 꺽쇠 img 사용하도록 수정

---
+++ src/components/Icon/VerticalAnchorArrow.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { VERTICAL_RIGHT_ARROW_ICON_URL } from 'src/constants/icons';
+
+export const VerticalAnchorArrow: React.FC = () => (
+  <img width={11} height={14} src={VERTICAL_RIGHT_ARROW_ICON_URL} alt={'페이지 이동'} />
+);
--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -2,8 +2,10 @@ import AdultBadge from 'src/assets/svg/AdultBadge.svg';
 import StarRating from 'src/assets/svg/StarRating.svg';
 import NoStarRating from 'src/assets/svg/NoStarRating.svg';
 import RidiWaitFree from 'src/assets/svg/RidiWaitFree.svg';
+import VerticalArrow from 'src/assets/svg/VerticalArrow.svg'
 
 export const ADULT_BADGE_URL = AdultBadge;
 export const STAR_RATING_URL = StarRating;
 export const NO_STAR_RATING_URL = NoStarRating;
 export const RIDI_WAIT_FREE_ICON_URL = RidiWaitFree;
+export const VERTICAL_RIGHT_ARROW_ICON_URL = VerticalArrow;
