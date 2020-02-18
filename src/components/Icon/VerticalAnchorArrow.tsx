62f7f3906302ad00f6eaea03bf8b86b15fe5bb8f
JeongSik Yun <jeongsik@ridi.com>
2020-02-18 15:04:36 +0900

    마진 넣을 수 있는 ArrowV Wrapping

--- src/components/Icon/VerticalAnchorArrow.tsx
+++ src/components/Icon/VerticalAnchorArrow.tsx
@@ -1,6 +1,35 @@
 import React from 'react';
 import { VERTICAL_RIGHT_ARROW_ICON_URL } from 'src/constants/icons';
+import styled from '@emotion/styled';
 
-export const VerticalAnchorArrow: React.FC = () => (
-  <img width={11} height={14} src={VERTICAL_RIGHT_ARROW_ICON_URL} alt={'페이지 이동'} />
+interface MarginProps {
+  marginTop?: number;
+  marginLeft?: number;
+  marginBottom?: number;
+  marginRight?: number;
+}
+
+const SpacingImg = styled.img<MarginProps>`
+  margin-top: ${(props) => props.marginTop}px;
+  margin-bottom: ${(props) => props.marginBottom}px;
+  margin-left: ${(props) => props.marginLeft}px;
+  margin-right: ${(props) => props.marginRight}px;
+`;
+
+export const VerticalAnchorArrow: React.FC<MarginProps> = ({
+  marginBottom = 0,
+  marginLeft = 0,
+  marginRight = 0,
+  marginTop = 0,
+}) => (
+  <SpacingImg
+    marginTop={marginTop}
+    marginBottom={marginBottom}
+    marginLeft={marginLeft}
+    marginRight={marginRight}
+    width={11}
+    height={14}
+    src={VERTICAL_RIGHT_ARROW_ICON_URL}
+    alt="페이지 이동"
+  />
 );
