ff5cf1077bc657616b7272410efc143854e31905
Heejae Jang <j132134@gmail.com>
2020-02-21 18:30:11 +0900

    Update next.config.js [!! DONT SQUASH for diff]

--- next.config.js
+++ next.config.js
@@ -20,6 +20,140 @@ dotenvLoad();
 const ENVIRONMENT = process.env.ENVIRONMENT || 'production';
 const STATIC_CDN_URL = process.env.STATIC_CDN_URL || 'https://books.ridicdn.net';
 
+const nextConfig = {
+  assetPrefix: STATIC_CDN_URL,
+  compress: false,
+  distDir: 'build',
+  exportPathMap: () => ({}),
+  experimental: {
+    redirects() {
+      return [
+        {
+          source: "/general/:path*",
+          destination: "/:path*",
+          permanent: true,
+        },
+      ];
+    },
+    rewrites() {
+      return [
+        ...['', 'bestsellers'].map(path => ({
+          source: `/${path}`,
+          destination: `/general/${path}`,
+        })),
+      ];
+    },
+  },
+  webpack (config, { buildId, isServer, webpack }) {
+    config.output.publicPath = STATIC_CDN_URL + '/_next/';
+
+    const modifyEntries = entries => {
+      if (!('main.js' in entries)) {
+        return entries;
+      }
+      if (typeof entries['main.js'] === 'string') {
+        entries['main.js'] = [entries['main.js']];
+      }
+      const entry = entries['main.js'];
+      if (!entry.includes('@babel/polyfill/noConflict')) {
+        entry.unshift('@babel/polyfill/noConflict');
+      }
+      if (!entry.includes('intersection-observer')) {
+        entry.unshift('intersection-observer');
+      }
+      return entries;
+    };
+    if (typeof config.entry === 'function') {
+      const prevEntry = config.entry;
+      config.entry = () => {
+        const entries = prevEntry();
+        if (typeof entries.then === 'function') {
+          return entries.then(modifyEntries);
+          // eslint-disable-next-line no-else-return
+        } else {
+          return modifyEntries(entries);
+        }
+      };
+    } else {
+      modifyEntries(config.entry);
+    }
+
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]',
+        },
+      },
+    });
+
+    if (!['local', 'profile'].includes(ENVIRONMENT)) {
+      config.plugins.push(
+        new SentryCliPlugin({
+          include: ['./build/', './src/'],
+          release: buildId,
+          urlPrefix: '~/_next/',
+          ignoreFile: '.sentrycliignore',
+          entries: [],
+          ignore: ['coverage', 'server', 'node_modules', 'webpack.config.js'],
+          rewrite: true,
+        }),
+      );
+    }
+    // config.plugins.push(
+    //   new CopyPlugin([
+    //     {
+    //       from: '../static/manifest.webmanifest',
+    //       to: '',
+    //       transform(content) {
+    //         return Promise.resolve(
+    //           Buffer.from(content, 'utf8')
+    //             .toString()
+    //             .replace(/<path>/gi, STATIC_CDN_URL),
+    //         );
+    //       },
+    //     },
+    //   ]),
+    // );
+    config.plugins.push(
+      new DotenvPlugin({
+        systemvars: true,
+        silent: true,
+      }),
+    );
+    config.plugins.push(
+      new InjectManifest({
+        swSrc: 'public/static/service-worker.js',
+        exclude: [
+          /\.map$/,
+          /\/pages\/partials\//,
+          /build-manifest\.json/,
+          /manifest\.webmanifest/,
+        ],
+      }),
+    );
+
+    config.plugins.push(new webpack.DefinePlugin({
+      'process.env.IS_SERVER': JSON.stringify(isServer),
+    }));
+
+    config.plugins.push(
+      new DotenvPlugin({
+        systemvars: true,
+        silent: true,
+      }),
+    );
+
+    config.node = {
+      net: 'empty',
+      fs: 'empty',
+    };
+    return config;
+  },
+};
+
 module.exports = withPlugins(
   [
     [withBundleAnalyzer, {
@@ -47,137 +181,5 @@ module.exports = withPlugins(
     [nextEnv()],
     [nextSourceMaps],
   ],
-  {
-        assetPrefix: STATIC_CDN_URL,
-        compress: false,
-        distDir: 'build',
-        exportPathMap: () => ({}),
-        experimental: {
-          redirects() {
-            return [
-              {
-                source: "/general/:path*",
-                destination: "/:path*",
-                permanent: true,
-              },
-            ];
-          },
-          rewrites() {
-            return [
-              ...['', 'bestsellers'].map(path => ({
-                source: `/${path}`,
-                destination: `/general/${path}`,
-              })),
-            ];
-          },
-        },
-        webpack (config, { buildId, isServer, webpack }) {
-          config.output.publicPath = STATIC_CDN_URL + '/_next/';
-
-          const modifyEntries = entries => {
-            if (!('main.js' in entries)) {
-              return entries;
-            }
-            if (typeof entries['main.js'] === 'string') {
-              entries['main.js'] = [entries['main.js']];
-            }
-            const entry = entries['main.js'];
-            if (!entry.includes('@babel/polyfill/noConflict')) {
-              entry.unshift('@babel/polyfill/noConflict');
-            }
-            if (!entry.includes('intersection-observer')) {
-              entry.unshift('intersection-observer');
-            }
-            return entries;
-          };
-          if (typeof config.entry === 'function') {
-            const prevEntry = config.entry;
-            config.entry = () => {
-              const entries = prevEntry();
-              if (typeof entries.then === 'function') {
-                return entries.then(modifyEntries);
-                // eslint-disable-next-line no-else-return
-              } else {
-                return modifyEntries(entries);
-              }
-            };
-          } else {
-            modifyEntries(config.entry);
-          }
-
-          config.module.rules.push({
-            test: /\.(eot|woff|woff2|ttf|png|jpg|gif)$/,
-            use: {
-              loader: 'url-loader',
-              options: {
-                limit: 100000,
-                name: '[name].[ext]',
-              },
-            },
-          });
-
-          if (!['local', 'profile'].includes(ENVIRONMENT)) {
-            config.plugins.push(
-              new SentryCliPlugin({
-                include: ['./build/', './src/'],
-                release: buildId,
-                urlPrefix: '~/_next/',
-                ignoreFile: '.sentrycliignore',
-                entries: [],
-                ignore: ['coverage', 'server', 'node_modules', 'webpack.config.js'],
-                rewrite: true,
-              }),
-            );
-          }
-          // config.plugins.push(
-          //   new CopyPlugin([
-          //     {
-          //       from: '../static/manifest.webmanifest',
-          //       to: '',
-          //       transform(content) {
-          //         return Promise.resolve(
-          //           Buffer.from(content, 'utf8')
-          //             .toString()
-          //             .replace(/<path>/gi, STATIC_CDN_URL),
-          //         );
-          //       },
-          //     },
-          //   ]),
-          // );
-          config.plugins.push(
-            new DotenvPlugin({
-              systemvars: true,
-              silent: true,
-            }),
-          );
-          config.plugins.push(
-            new InjectManifest({
-              swSrc: 'public/static/service-worker.js',
-              exclude: [
-                /\.map$/,
-                /\/pages\/partials\//,
-                /build-manifest\.json/,
-                /manifest\.webmanifest/,
-              ],
-            }),
-          );
-
-          config.plugins.push(new webpack.DefinePlugin({
-            'process.env.IS_SERVER': JSON.stringify(isServer),
-          }));
-
-          config.plugins.push(
-            new DotenvPlugin({
-              systemvars: true,
-              silent: true,
-            }),
-          );
-
-          config.node = {
-            net: 'empty',
-            fs: 'empty',
-          };
-          return config;
-        },
-  },
+  nextConfig,
 );
