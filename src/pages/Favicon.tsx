40e344c47fc4601c2eafa74377960a855069f104
Heejae Jang <j132134@gmail.com>
2020-02-25 16:28:56 +0900

    Move /public/static/favicon into /src/assets

--- src/pages/Favicon.tsx
+++ src/pages/Favicon.tsx
@@ -1,19 +1,19 @@
-import faviconWin8 from '../../public/static/favicon/favicon_win8.png';
-import favicon from '../../public/static/favicon/favicon.ico';
-import appleIcon57 from '../../public/static/favicon/apple-icon-57x57.png';
-import appleIcon60 from '../../public/static/favicon/apple-icon-60x60.png';
-import appleIcon72 from '../../public/static/favicon/apple-icon-72x72.png';
-import appleIcon76 from '../../public/static/favicon/apple-icon-76x76.png';
-import appleIcon114 from '../../public/static/favicon/apple-icon-114x114.png';
-import appleIcon120 from '../../public/static/favicon/apple-icon-120x120.png';
-import appleIcon144 from '../../public/static/favicon/apple-icon-144x144.png';
-import appleIcon152 from '../../public/static/favicon/apple-icon-152x152.png';
-import appleIcon180 from '../../public/static/favicon/apple-icon-180x180.png';
-import androidIcon192 from '../../public/static/favicon/android-icon-192x192.png';
-import appleTouchIcon from '../../public/static/favicon/apple-touch-icon.png';
-import favicon32 from '../../public/static/favicon/favicon-32x32.png';
-import favicon16 from '../../public/static/favicon/favicon-16x16.png';
-import androidChrome192 from '../../public/static/favicon/android-chrome-192x192.png';
+import faviconWin8 from 'src/assets/favicon/favicon_win8.png';
+import favicon from 'src/assets/favicon/favicon.ico';
+import appleIcon57 from 'src/assets/favicon/apple-icon-57x57.png';
+import appleIcon60 from 'src/assets/favicon/apple-icon-60x60.png';
+import appleIcon72 from 'src/assets/favicon/apple-icon-72x72.png';
+import appleIcon76 from 'src/assets/favicon/apple-icon-76x76.png';
+import appleIcon114 from 'src/assets/favicon/apple-icon-114x114.png';
+import appleIcon120 from 'src/assets/favicon/apple-icon-120x120.png';
+import appleIcon144 from 'src/assets/favicon/apple-icon-144x144.png';
+import appleIcon152 from 'src/assets/favicon/apple-icon-152x152.png';
+import appleIcon180 from 'src/assets/favicon/apple-icon-180x180.png';
+import androidIcon192 from 'src/assets/favicon/android-icon-192x192.png';
+import appleTouchIcon from 'src/assets/favicon/apple-touch-icon.png';
+import favicon32 from 'src/assets/favicon/favicon-32x32.png';
+import favicon16 from 'src/assets/favicon/favicon-16x16.png';
+import androidChrome192 from 'src/assets/favicon/android-chrome-192x192.png';
 
 export default () => (
   <>
