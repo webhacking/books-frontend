fc3067a3c9dc3d4fabaee468225824876208529d
오민형 <minhyung@ridi.com>
2020-03-03 16:48:51 +0900

    Instant Search Result style 정리

--- src/components/Search/InstantSearchResult.tsx
+++ src/components/Search/InstantSearchResult.tsx
@@ -4,6 +4,7 @@ import { View } from 'libreact/lib/View';
 import { WindowWidthQuery } from 'libreact/lib/WindowWidthQuery';
 import { lineClamp, RIDITheme } from 'src/styles';
 import styled from '@emotion/styled';
+import * as colors from '@ridi/colors';
 import { getEscapedString } from 'src/utils/highlight';
 import { BreakPoint, greaterThanOrEqualTo, orBelow } from 'src/utils/mediaQuery';
 import {
@@ -22,7 +23,8 @@ const listItemCSS = css`
   )};
   cursor: pointer;
 `;
-const bookListItemCSS = (theme: RIDITheme) => css`
+
+const BookListItem = styled.li`
   ${listItemCSS};
   ${orBelow(
     BreakPoint.LG,
@@ -48,22 +50,22 @@ const bookListItemCSS = (theme: RIDITheme) => css`
         }
 
         :hover {
-          background-color: ${theme.instantSearch.itemHover};
+          background-color: ${colors.lightSteelBlue5};
         }
       `,
   )}
     :hover {
-    background-color: ${theme.instantSearch.itemHover};
+    background-color: ${colors.lightSteelBlue5};
   }
   button {
     :focus {
-      background-color: ${theme.instantSearch.itemHover};
+      background-color: ${colors.lightSteelBlue5};
       outline: none !important;
     }
   }
 `;
 
-const authorListItemCSS = (theme: RIDITheme) => css`
+const AuthorListItem = styled.li`
   ${listItemCSS};
   display: flex;
   ${orBelow(
@@ -87,24 +89,136 @@ const authorListItemCSS = (theme: RIDITheme) => css`
     `,
   )};
   :hover {
-    background-color: ${theme.instantSearch.itemHover};
+    background-color: ${colors.lightSteelBlue5};
   }
   button {
     :focus {
-      background-color: ${theme.instantSearch.itemHover};
+      background-color: ${colors.lightSteelBlue5};
       outline: none !important;
     }
   }
 `;
 
-const titleCSS = css`
+const AuthorListItemButton = styled.button`
+  width: 100%;
+  height: 100%;
+  padding: 13px 16px;
+  text-align: left;
+  flex-wrap: wrap;
+`;
+
+const BookListItemButton = styled.button`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  text-align: left;
+  padding: 14px 16px;
+`;
+
+const BookThumbnail = styled.img`
+  margin-right: 12px;
+  border: 1px solid ${colors.slateGray30};
+  flex-shrink: 0;
+  width: 38px;
+`;
+
+const BookMeta = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  text-align: left;
+  justify-content: center;  
+`;
+
+const BookTitle = styled.span`
   font-size: 15px;
   line-height: 1.33;
   word-break: keep-all;
+  margin-right: 4px;
+  margin-bottom: 3px;
+  color: #252525; 
+  ${greaterThanOrEqualTo(
+    BreakPoint.LG + 1,
+    css`
+      max-width: 298px;
+      color: #303538;
+      margin-bottom: 4px;
+    `,
+  )};
+`;
+
+const BookAuthors = styled.div`
+  word-break: keep-all;
+  ${greaterThanOrEqualTo(
+    BreakPoint.LG + 1,
+    css`
+      max-width: 298px;
+    `,
+  )}
+`;
+
+const authorPublisherCSS = css`
+  font-size: 14px;
+  line-height: 1.36;
+  color: #808991;
+  -webkit-font-smoothing: antialiased;
+  max-width: 298px;
+`;
+
+const AuthorInfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const AuthorName = styled.span`
+  font-size: 15px;
+  line-height: 1.33;
+  flex-shrink: 0;
+  margin-right: 8px;
+  ${orBelow(
+    BreakPoint.LG,
+    css`
+      margin-right: 3px;
+    `,
+  )};
+  color: #303538;
+`;
+
+const AuthorBooksInfo = styled.span`
+  font-size: 14px;
+  word-break: keep-all;
+  color: ${colors.slateGray50};
+  ${lineClamp(1)};
+`;
+
+const Author = styled.span`
+  ${authorPublisherCSS};
+  margin-right: 4px;
+`;
+
+const AuthorPublisher = styled.span`
+  ${authorPublisherCSS};
+  padding-left: 4px;
+  border-left: 1px solid #e6e8eb;
+  ${greaterThanOrEqualTo(
+    BreakPoint.LG + 1,
+    css`
+      max-width: 298px;
+    `,
+  )}
 `;
 
 const ItemWrapper = styled.div`
   display: flex;
+`;
+
+const InstantSearchDivider = styled.hr`
+  height: 1px;
+  border: 0;
+  border-top: 1px solid #e6e8e0;
+  margin: 8px 16px;
 `;
 
 interface InstantSearchResultProps {
@@ -125,54 +239,19 @@ const AuthorInfo: React.FC<{ author: InstantSearchAuthorResultScheme }> = (props
   const { author } = props;
 
   return (
-    <div
-      css={css`
-        display: flex;
-        align-items: center;
-        width: 100%;
-      `}
-    >
-      <span
-        css={css`
-          font-size: 15px;
-          line-height: 1.33;
-          flex-shrink: 0;
-          ${orBelow(
-          BreakPoint.LG,
-          css`
-              margin-right: 3px;
-            `,
-        )};
-          margin-right: 8px;
-          color: #303538;
-        `}
+    <AuthorInfoWrapper>
+      <AuthorName
         dangerouslySetInnerHTML={{
           __html: getEscapedString(author.highlight.name_raw || author.name_raw),
         }}
       />
-      <span
-        css={(theme: RIDITheme) => css`
-          font-size: 14px;
-          word-break: keep-all;
-          color: ${theme.label2};
-          ${lineClamp(1)};
-        `}
-      >
-        {`<${author.popular_book_title}>${
-          author.book_count > 1 ? ` 외 ${author.book_count - 1}권` : ''
-        }`}
-      </span>
-    </div>
+      <AuthorBooksInfo>
+        {`<${author.popular_book_title}>`}
+        {author.book_count > 1 ? ` 외 ${author.book_count - 1}권` : ''}
+      </AuthorBooksInfo>
+    </AuthorInfoWrapper>
   );
 };
-
-const authorPublisherCSS = css`
-  font-size: 14px;
-  line-height: 1.36;
-  color: #808991;
-  -webkit-font-smoothing: antialiased;
-  max-width: 298px;
-`;
 
 // Todo 사용 컴포넌트마다 다른 options 사용해서 보여주기
 const AuthorLabel: React.FC<{ author: string; authors: AuthorInfoScheme[] }> = (props) => {
@@ -182,28 +261,18 @@ const AuthorLabel: React.FC<{ author: string; authors: AuthorInfoScheme[] }> = (
       .map((author) => author.name);
   if (!viewedAuthors || viewedAuthors.length === 0) {
     return (
-      <span
-        css={css`
-          ${authorPublisherCSS};
-          margin-right: 4px;
-        `}
-      >
+      <Author>
         {props.author}
-      </span>
+      </Author>
     );
   }
 
   return (
-    <span
-      css={css`
-        ${authorPublisherCSS};
-        margin-right: 4px;
-      `}
-    >
+    <Author>
       {viewedAuthors.length > 2
         ? `${viewedAuthors[0]} 외 ${viewedAuthors.length - 1}명`
         : `${viewedAuthors.join(', ')}`}
-    </span>
+    </Author>
   );
 };
 
@@ -214,30 +283,16 @@ const BookList: React.FC<InstantSearchResultBookListProps> = React.memo((props) 
     <>
       <ul>
         {result.books.map((book: InstantSearchBookResultScheme, index) => (
-          <li data-book-id={book.b_id} css={bookListItemCSS} key={index}>
-            <button
-              css={css`
-                height: 100%;
-                width: 100%;
-                display: flex;
-                flex-wrap: wrap;
-                //align-items: center;
-                text-align: left;
-                padding: 14px 16px;
-              `}
+          <BookListItem data-book-id={book.b_id} key={index}>
+            <BookListItemButton
+              type="button"
               data-book-id={book.b_id}
               onKeyDown={handleKeyDown}
               onClick={handleClickBookItem}
             >
               <WindowWidthQuery>
                 <View maxWidth={1000}>
-                  <span
-                    css={css`
-                      ${titleCSS};
-                      margin-right: 4px;
-                      margin-bottom: 3px;
-                      color: #252525;
-                    `}
+                  <BookTitle
                     dangerouslySetInnerHTML={{
                       __html: getEscapedString(
                         book.highlight.web_title_title_raw || book.web_title_title_raw,
@@ -245,95 +300,34 @@ const BookList: React.FC<InstantSearchResultBookListProps> = React.memo((props) 
                     }}
                   />
                   <AuthorLabel author={book.author} authors={book.authors_info} />
-                  <span
-                    css={css`
-                      ${authorPublisherCSS};
-                      padding-left: 4px;
-                      border-left: 1px solid #e6e8eb;
-                      ${greaterThanOrEqualTo(
-                      BreakPoint.LG + 1,
-                      css`
-                          max-width: 298px;
-                        `,
-                    )}
-                    `}
-                  >
-                    {book.publisher}
-                  </span>
+                  <AuthorPublisher>{book.publisher}</AuthorPublisher>
                 </View>
                 <View>
-                  <div>
-                    <ItemWrapper>
-                      <img
-                        alt={book.web_title_title}
-                        css={(theme: RIDITheme) => css`
-                          margin-right: 12px;
-                          border: 1px solid ${theme.image.border};
-                          flex-shrink: 0;
-                          width: 38px;
-                        `}
-                        width="38px"
-                        height="58px"
-                        src={`https://img.ridicdn.net/cover/${book.b_id}/small`}
+                  <ItemWrapper>
+                    <BookThumbnail
+                      alt={book.web_title_title}
+                      width="38px"
+                      height="58px"
+                      src={`https://img.ridicdn.net/cover/${book.b_id}/small`}
+                    />
+                    <BookMeta>
+                      <BookTitle
+                        dangerouslySetInnerHTML={{
+                          __html: getEscapedString(
+                            book.highlight.web_title_title_raw || book.web_title_title_raw,
+                          ),
+                        }}
                       />
-                      <div
-                        css={css`
-                          display: flex;
-                          flex-direction: column;
-                          align-items: start;
-                          text-align: left;
-                          justify-content: center;
-                        `}
-                      >
-                        <p
-                          css={css`
-                            ${titleCSS};
-                            color: #303538;
-                            margin-bottom: 4px;
-                            word-break: keep-all;
-                            ${greaterThanOrEqualTo(
-                            BreakPoint.LG + 1,
-                            css`
-                                max-width: 298px;
-                              `,
-                          )}
-                          `}
-                          dangerouslySetInnerHTML={{
-                            __html: getEscapedString(
-                              book.highlight.web_title_title_raw
-                                || book.web_title_title_raw,
-                            ),
-                          }}
-                        />
-                        <div
-                          css={css`
-                            word-break: keep-all;
-                            ${greaterThanOrEqualTo(
-                            BreakPoint.LG + 1,
-                            css`
-                                max-width: 298px;
-                              `,
-                          )}
-                          `}
-                        >
-                          <AuthorLabel author={book.author} authors={book.authors_info} />
-                          <span
-                            css={css`
-                              ${authorPublisherCSS};
-                              padding-left: 4px;
-                              border-left: 1px solid #e6e8eb;
-                            `}
-                          >
-                            {book.publisher}
-                          </span>
-                        </div>
-                      </div>
-                    </ItemWrapper>
-                  </div>
+                      <BookAuthors>
+                        <AuthorLabel author={book.author} authors={book.authors_info} />
+                        <AuthorPublisher>{book.publisher}</AuthorPublisher>
+                      </BookAuthors>
+                    </BookMeta>
+                  </ItemWrapper>
                 </View>
               </WindowWidthQuery>
-            </button>
-          </li>
+            </BookListItemButton>
+          </BookListItem>
         ))}
       </ul>
     </>
@@ -367,15 +361,9 @@ const InstantSearchResult: React.FC<InstantSearchResultProps> = React.memo((prop
         <>
           <ul>
             {result.authors.map((author, index) => (
-              <li css={authorListItemCSS} key={index}>
-                <button
-                  css={css`
-                    width: 100%;
-                    height: 100%;
-                    padding: 13px 16px;
-                    text-align: left;
-                    flex-wrap: wrap;
-                  `}
+              <AuthorListItem key={index}>
+                <AuthorListItemButton
+                  type="button"
                   data-author-id={author.id}
                   onKeyDown={handleKeyDown}
                   onClick={handleClickAuthorItem}
@@ -385,25 +373,16 @@ const InstantSearchResult: React.FC<InstantSearchResultProps> = React.memo((prop
                       <AuthorInfo author={author} />
                     </View>
                     <View>
-                      <div>
-                        <ItemWrapper>
-                          <AuthorInfo author={author} />
-                        </ItemWrapper>
-                      </div>
+                      <ItemWrapper>
+                        <AuthorInfo author={author} />
+                      </ItemWrapper>
                     </View>
                   </WindowWidthQuery>
-                </button>
-              </li>
+                </AuthorListItemButton>
+              </AuthorListItem>
             ))}
           </ul>
-          <hr
-            css={css`
-              height: 1px;
-              border: 0;
-              border-top: 1px solid #e6e8e0;
-              margin: 8px 16px;
-            `}
-          />
+          <InstantSearchDivider />
         </>
       )}
       {result.books.length > 0 && (
