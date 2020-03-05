7cc23521feba07e5fbf3486b791cb4f781327adc
Heejae Jang <j132134@gmail.com>
2020-03-05 13:35:40 +0900

    Use lagacy manifest.webmanifest url in production stage

--- src/components/Meta/index.tsx
+++ src/components/Meta/index.tsx
@@ -32,7 +32,11 @@ export default () => (
       name="description"
       content="웹툰/웹소설, 전자책, 만화까지 취향에 딱 맞는 콘텐츠를 제안합니다."
     />
-    <link rel="manifest" href="/manifest.webmanifest" />
+    {process.env.STAGE === 'production' ? (
+      <link rel="manifest" href="https://books.ridicdn.net/_next/manifest.webmanifest" />
+    ) : (
+      <link rel="manifest" href="/manifest.webmanifest" />
+    )}
     <link
       href="https://fonts.googleapis.com/css?family=Nanum+Gothic&display=swap&subset=korean"
       rel="stylesheet"
