0473672427abdb39b4a412b5aa2895860d062120
JeongSik Yun <jeongsik@ridi.com>
2020-03-05 21:15:26 +0900

    테스트 추가

--- src/tests/components/Meta/Meta.spec.tsx
+++ src/tests/components/Meta/Meta.spec.tsx
@@ -1,10 +1,28 @@
 import * as React from 'react';
 import Meta from 'src/components/Meta';
-import { render, cleanup } from '@testing-library/react';
+import CSP from 'src/components/Meta/CSP';
+
+import { render, cleanup, waitForElement } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-afterEach(cleanup);
 
-test('should be render Index Component', () => {
-  render(<Meta />);
+describe('testing Meta & Head', () => {
+  afterEach(cleanup);
+
+  it('should be render Meta Component', async () => {
+    const { container } = render(<Meta />);
+
+    expect(container).not.toBe(null);
+  });
+
+  it('should be render CSP meta', async () => {
+    const { container } = render(<CSP />);
+    const meta = await waitForElement(() => container, { container });
+    const cspMeta = meta.querySelector('meta');
+    expect(cspMeta.httpEquiv).toBe('Content-Security-Policy');
+  });
+
 });
+
+
+
