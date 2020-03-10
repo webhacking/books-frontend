c0ab22325c7d087f0f64b6d3e904e0e3d1405ebe
오민형 <minhyung@ridi.com>
2020-03-10 12:54:39 +0900

    deviceType 수정

--- src/components/Context/DeviceType.tsx
+++ src/components/Context/DeviceType.tsx
@@ -10,9 +10,9 @@ export const DeviceType: React.FC = (props) => {
   const [deviceType, setDeviceType] = useState(null);
   const [isMobile, setIsMobile] = useState(false);
   useEffect(() => {
-    const device = getDeviceType();
+    const device = ['mobile', 'tablet'].includes(getDeviceType()) ? 'mobile' : 'pc';
     setDeviceType(device);
-    setIsMobile(['mobile', 'tablet'].includes(device));
+    setIsMobile(device === 'mobile');
   }, []);
   return (
     <DeviceTypeContext.Provider value={{ deviceType, isMobile }}>{props.children}</DeviceTypeContext.Provider>
