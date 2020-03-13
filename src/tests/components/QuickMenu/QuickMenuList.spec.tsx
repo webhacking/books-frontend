02deac7317809b592cbe0e846d5100adce460720
JeongSik Yun <jeongsik@ridi.com>
2020-03-13 14:04:56 +0900

    테스트 수정

--- src/tests/components/QuickMenu/QuickMenuList.spec.tsx
+++ src/tests/components/QuickMenu/QuickMenuList.spec.tsx
@@ -5,24 +5,39 @@ import '@testing-library/jest-dom/extend-expect';
 // @ts-ignore
 import { ThemeProvider } from 'emotion-theming';
 import { defaultTheme } from 'src/styles';
+import makeStore from '../../../store/config';
+import { Provider } from 'react-redux';
 
 afterEach(cleanup);
+const store = makeStore(
+  {
+    books: {
+      itmes: {
+        '12345': null,
+      },
+      isFetching: false,
+    },
+  },
+  { asPath: 'test', isServer: false },
+);
 
 const renderComponent = () =>
   render(
     <ThemeProvider theme={defaultTheme}>
+      <Provider store={store}>
       <QuickMenuList
         items={[
-          { id: 1, name: '신간', icon: '/', bg_color: '#ffffff', url: '/', order: 1 },
+          { id: 1, name: '이벤트 만세', icon: '/', bg_color: '#ffffff', url: '/event/12345', order: 1 },
         ]}
       />
+      </Provider>
     </ThemeProvider>,
   );
 
-describe('test instant search', () => {
-  it('should be render input', () => {
+describe('test QuickMenuList Component', () => {
+  it('should be render QuickMenu Item', () => {
     const { container } = renderComponent();
-    const itemNode = getByText(container, '신간');
-    expect(itemNode).not.toBe(null);
+    const itemNode = getByText(container, '이벤트 만세');
+    expect(itemNode[0]).not.toBe(null);
   });
 });
