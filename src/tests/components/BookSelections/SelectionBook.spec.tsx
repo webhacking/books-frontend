366514e2360991fa2e6901503218f9c7824d9dd2
JeongSik Yun <jeongsik@ridi.com>
2020-03-24 18:34:13 +0900

    추천 제외 렌더 테스트 추가

--- src/tests/components/BookSelections/SelectionBook.spec.tsx
+++ src/tests/components/BookSelections/SelectionBook.spec.tsx
@@ -1,5 +1,13 @@
 import * as React from 'react';
-import { act, render, cleanup, getAllByAltText, RenderResult, waitForElement } from '@testing-library/react';
+import {
+  act,
+  render,
+  cleanup,
+  getAllByAltText,
+  getAllByText,
+  RenderResult,
+  waitForElement,
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { ThemeProvider } from 'emotion-theming';
 import { Provider } from 'react-redux';
@@ -14,21 +22,21 @@ const store = makeStore(
     books: {
       itmes: {
         '12345666': {
-          b_id: '12345666'
-        }
+          b_id: '12345666',
+        },
       },
       isFetching: false,
     },
   },
   { asPath: 'test', isServer: false },
 );
-const renderSelectionBookList = () =>
+const renderSelectionBookList = (listOptions : { aiRecommendation: boolean }) =>
   render(
     <ThemeProvider theme={defaultTheme}>
       <Provider store={store}>
         <ViewportIntersectionProvider>
           <SelectionBookList
-            isAIRecommendation={false}
+            isAIRecommendation={listOptions.aiRecommendation}
             items={[
               {
                 b_id: '12345666',
@@ -69,8 +77,14 @@ describe('test SelectionBookContainer', () => {
   });
 
   it('should be render SelectionBookList item', async () => {
-    const { container } = actRender(renderSelectionBookList);
+    const { container } = actRender(() => renderSelectionBookList({aiRecommendation: false}));
     const item = await waitForElement(() => getAllByAltText(container, '도서 표지'));
+    expect(item).not.toBe(null);
+  });
+
+  it('should be render 추천제외 버튼 for AI Recommendation', async () => {
+    const { container } = actRender(() => renderSelectionBookList({aiRecommendation: true}));
+    const item = await waitForElement(() => getAllByText(container, '추천 제외'));
     expect(item).not.toBe(null);
   });
 });
