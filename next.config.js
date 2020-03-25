ba4bcef0a27f5d15e11a9811f60fd77b6beb0f1c
Heejae Jang <j132134@gmail.com>
2020-03-26 00:06:27 +0900

    Rename: `NEXT_STATIC_SENTRY_DSN` -> `SENTRY_DSN`

--- next.config.js
+++ next.config.js
@@ -81,6 +81,7 @@ const nextConfig = {
     config.plugins.push(new webpack.DefinePlugin({
       'process.env.USE_CSR': JSON.stringify(process.env.USE_CSR),
       'process.env.STAGE': JSON.stringify(process.env.STAGE),
+      'process.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
       'process.env.BUILD_ID': JSON.stringify(buildId),
       'process.env.IS_SERVER': JSON.stringify(isServer),
       'process.env.IS_PRODUCTION': JSON.stringify(process.env.NODE_ENV === 'production'),
