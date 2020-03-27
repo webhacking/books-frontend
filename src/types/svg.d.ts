5a20384c73754192d0c5f3ff2c70584c061fdb5b
Wonwoo Choi <wonwoo.choi@ridi.com>
2020-03-27 16:19:52 +0900

    inline svg와 rsvg를 구분해서 정의

--- src/types/svg.d.ts
+++ src/types/svg.d.ts
@@ -1,4 +1,9 @@
-declare module '*.svg' {
+declare module 'svgs/*.svg' {
   const content: React.ComponentType<React.SVGProps<SVGElement>>;
+  export default content;
+}
+
+declare module 'assets/svg/*.svg' {
+  const content: string;
   export default content;
 }
