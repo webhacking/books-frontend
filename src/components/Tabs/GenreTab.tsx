71a81ca46598be391799e0e7c797656c26ad21c3
Wonwoo Choi <wonwoo.choi@ridi.com>
2020-03-31 17:02:51 +0900

    Revert "Merge pull request #173 from ridi/hotfix/force-href-to-general"
    This reverts commit 19d880bb5d1b36a900667174ecb25814786c51dc, reversing
    changes made to f04a6e56129add04af69e57210e4832c4b70706a.

--- src/components/Tabs/GenreTab.tsx
+++ src/components/Tabs/GenreTab.tsx
@@ -360,7 +360,7 @@ const GenreTab: React.FC<GenreTabProps> = React.memo((props) => {
                 <span className="a11y">{labels.category}</span>
               </a>
             </GenreListItem>
-            <TabItem activePath={/^\/?$/} label="일반" href="/general" />
+            <TabItem activePath={/^\/?$/} label="일반" href="/" />
             <TabItem
               activePath={/^\/romance(-serial)?\/?$/}
               label="로맨스"
