dfc963e9193ec8bb4f8ee0c2fa855907eba4bb4e
오민형 <minhyung@ridi.com>
2020-04-02 17:24:06 +0900

    인앱에서 나눔고딕 제거

--- src/components/Misc/InAppThemeProvider.tsx
+++ src/components/Misc/InAppThemeProvider.tsx
@@ -9,6 +9,7 @@ const InAppStyle = (theme: RIDITheme) => css`
   * { -webkit-tap-highlight-color: rgba(255,255,255,0); }
   html {
     background-color: ${theme.backgroundColor};
+    font-family: ridi-roboto, Apple SD Gothic Neo, "돋움", Dotum, Helvetica Neue, arial, sans-serif;
   }
 `;
 
