2b951a5cec8b717a3ba8257d39b1379903740cca
forsyphilis <forsyphilis@gmail.com>
2020-04-06 12:21:16 +0900

    통합 검색 결과 타입 추가

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,27 +1,54 @@
 import * as React from 'react';
 import Head from 'next/head';
 import { ConnectedInitializeProps } from 'src/types/common';
+import styled from '@emotion/styled';
+import axios from 'src/utils/axios';
+import * as SearchTypes from './types';
 
 interface SearchProps {
   q?: string;
+  result: SearchTypes.SearchResult;
 }
 
-export default class Search extends React.Component<SearchProps> {
-  public static async getInitialProps(props: ConnectedInitializeProps) {
-    return { q: props.query.q };
-  }
+const SearchResultSection = styled.section`
+  max-width: 952px;
+  margin: 0 auto;
+`;
 
-  public render() {
-    return (
-      <>
-        <Head>
-          <title>리디북스 - 검색</title>
-        </Head>
-        <div>
-검색 결과
-          {this.props.q}
-        </div>
-      </>
-    );
-  }
+function SearchPage(props: SearchProps) {
+  return (
+    <SearchResultSection>
+      <Head>
+        <title>
+          {props.q}
+          {' '}
+          검색 결과 - 리디북스
+        </title>
+      </Head>
+      <div>검색 결과</div>
+    </SearchResultSection>
+  );
 }
+
+SearchPage.getInitialProps = async (props: ConnectedInitializeProps) => {
+  const {
+    req, isServer, res, store, query,
+  } = props;
+  const searchKeyword = query.q ?? '';
+  const searchUrl = new URL('/search', process.env.NEXT_STATIC_SEARCH_API);
+  searchUrl.searchParams.append('site', 'ridi-store');
+  searchUrl.searchParams.append('where', 'author');
+  searchUrl.searchParams.append('where', 'book');
+  searchUrl.searchParams.append('what', 'base');
+  searchUrl.searchParams.append('keyword', searchKeyword as string);
+  if (isServer) {
+    const searchResult = await axios.get<SearchTypes.SearchResult>(searchUrl.toString());
+    // const result = await pRetry(() => axios.get(process.env.NEXT_STATIC_SEARCH_API), {
+    //   retries: 3,
+    // });
+    // console.log(result, q);
+  }
+  return { q: props.query.q };
+};
+
+export default SearchPage;
