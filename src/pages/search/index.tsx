2e12c0d9146874d76b49441de13385ad9986c727
forsyphilis <forsyphilis@gmail.com>
2020-04-08 13:46:23 +0900

    작가 목록 스타일링 추가

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -5,6 +5,7 @@ import styled from '@emotion/styled';
 import axios from 'src/utils/axios';
 import * as SearchTypes from 'src/types/searchResults';
 import { AuthorInfo } from 'src/components/Search/InstantSearchResult';
+import { slateGray40, slateGray90 } from '@ridi/colors';
 
 interface SearchProps {
   q?: string;
@@ -18,13 +19,45 @@ const SearchResultSection = styled.section`
   margin: 0 auto;
 `;
 
-function AuthorList(props: { author: SearchTypes.AuthorResult }) {
+const SearchTitle = styled.h3`
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 21px;
+  color: ${slateGray90};
+  display: flex;
+  align-items: center;
+  padding: 10px 0;
+`;
+
+const TotalAuthor = styled.span`
+  font-size: 13px;
+  font-weight: normal;
+  margin-left: 5px;
+  color: ${slateGray40};
+`;
+
+const AuthorItem = styled.li`
+  padding: 12px 0;
+  display: flex;
+  align-items: center;
+`;
+
+const AuthorList = styled.ul`
+  margin-bottom: 16px;
+`;
+
+function Authors(props: { author: SearchTypes.AuthorResult }) {
+  const [isCollapsed, setCollapse] = React.useState(props.author.total > 30);
+
   return (
-    <ul>
+    <AuthorList>
       {props.author.authors.map((author) => (
-        <AuthorInfo author={author} />
+        <AuthorItem key={author.id}>
+          <AuthorInfo author={author} />
+        </AuthorItem>
       ))}
-    </ul>
+      {isCollapsed && <span>ho</span>}
+    </AuthorList>
   );
 }
 
@@ -41,11 +74,25 @@ function SearchPage(props: SearchProps) {
           검색 결과 - 리디북스
         </title>
       </Head>
-      {props.author.total > 0 && <AuthorList author={author} />}
-      <h3>검색 결과</h3>
-      {props.book?.books.map((item) => (
-        <span key={item.b_id}>{item.title}</span>
-      ))}
+      {props.author.total > 0 && (
+        <>
+          <SearchTitle>
+            {`‘${q}’ 저자 검색 결과`}
+            <TotalAuthor>
+              {author.total > 30 ? '총 30명+' : `총 ${author.total}명`}
+            </TotalAuthor>
+          </SearchTitle>
+          <Authors author={author} />
+        </>
+      )}
+      {props.book.total > 0 && (
+        <>
+          <SearchTitle>{`‘${q}’ 도서 검색 결과`}</SearchTitle>
+          {props.book?.books.map((item) => (
+            <span key={item.b_id}>{item.title}</span>
+          ))}
+        </>
+      )}
     </SearchResultSection>
   );
 }
