bcb3e34a770222a0fae8d388dda2daeeb410e33a
forsyphilis <forsyphilis@gmail.com>
2020-04-10 18:04:17 +0900

    box-shadow 로 border 잡음

--- src/components/Tabs/SearchCategoryTab.tsx
+++ src/components/Tabs/SearchCategoryTab.tsx
@@ -15,7 +15,7 @@ interface SearchCategoryProps {
 const CategoryList = styled.ul`
   display: flex;
   max-width: 952px;
-  border-bottom: 1px solid ${slateGray20};
+  box-shadow: inset 0px -1px 0px ${slateGray20};
   overflow-x: auto;
   height: 45px;
   ${scrollBarHidden}
@@ -55,9 +55,7 @@ function SearchCategoryTab(props: SearchCategoryProps) {
               category.category_name,
             )}#${category.category_id}`}
           >
-            <CategoryAnchor
-              id={category.category_id.toString()}
-            >
+            <CategoryAnchor id={category.category_id.toString()}>
               {`${category.category_name} (${category.doc_count})`}
             </CategoryAnchor>
           </Link>
