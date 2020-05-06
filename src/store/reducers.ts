6188fa4a9577092b3e67a7f2835199746be0756d
Wonwoo Choi <wonwoo.choi@ridi.com>
2020-05-06 17:59:36 +0900

    connected-next-router 제거

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import { routerReducer } from 'connected-next-router';
 import { accountReducer } from 'src/services/accounts';
 import { booksReducer } from 'src/services/books';
 import { categoryReducer } from 'src/services/category';
 import { notificationReducer } from 'src/services/notification';
 
 export const rootReducers = combineReducers({
-  router: routerReducer,
   account: accountReducer,
   books: booksReducer,
   categories: categoryReducer,
