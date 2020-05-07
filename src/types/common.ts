cd48799365bdde36f87fd91e3839e86e43d1cbe6
Wonwoo Choi <wonwoo.choi@ridi.com>
2020-05-07 17:50:36 +0900

    사용하지 않는 패키지 정리

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,9 +1,7 @@
 import { Store } from 'redux';
 import { NextPageContext } from 'next';
-import { Request } from 'express';
 
 export interface ConnectedInitializeProps extends NextPageContext {
   store: Store;
-  req?: Request;
   isServer: boolean;
 }
