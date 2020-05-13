7495264d409ba7d2566a1958190970960d5b9ac7
Wonwoo Choi <wonwoo.choi@ridi.com>
2020-05-13 16:08:00 +0900

    테스트 추가

---
+++ src/tests/utils/books.spec.ts
@@ -0,0 +1,30 @@
+import { constructSearchDesc } from 'src/utils/books';
+
+describe('constructSearchDesc', () => {
+  it.each([
+    [{
+    }, ''],
+    [{
+      intro: 'intro',
+      toc: 'toc',
+    }, 'intro <목차> toc'],
+    [{
+      intro: 'intro',
+      author: 'author',
+      toc: 'toc',
+    }, 'intro <저자 소개> author <목차> toc'],
+    [{
+      intro: 'intro',
+      publisher_review: 'publisher',
+    }, 'intro <출판사 서평> publisher'],
+    [{
+      publisher_review: 'publisher',
+    }, '<출판사 서평> publisher'],
+    [{
+      intro: 'intro\r\nwith newline',
+      toc: 'toc\r\nwith <b>tags</b> &amp; entities',
+    }, 'intro with newline <목차> toc with tags & entities'],
+  ])('should construct description correctly', (description, expected) => {
+    expect(constructSearchDesc(description)).toBe(expected);
+  });
+});
