11b98ddd0bc643cf15a4fa07a54526663e5ae0f7
forsyphilis <forsyphilis@gmail.com>
2020-05-18 20:19:45 +0900

    리뷰 반영

--- src/components/Book/SearchLandscapeBook.tsx
+++ src/components/Book/SearchLandscapeBook.tsx
@@ -431,7 +431,7 @@ export function SearchLandscapeBook(props: SearchLandscapeBookProps) {
         <StyledThumbnailWithBadge
           bId={item.b_id}
           genre={genres[0] ?? ''}
-          slug={`search.result_book.${book.categories[0].genre ?? 'etc'}`}
+          slug={`search.result_book.${genres[0] ?? 'etc'}`}
           sizes="(min-width: 999px) 100px, 80px"
           title={title}
         />
