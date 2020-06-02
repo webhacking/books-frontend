8791aea1313490dcd87265524446d912b93ad491
Wonwoo Choi <wonwoo.choi@ridi.com>
2020-06-02 20:41:45 +0900

    AIRecommendationBook 타입 사용처 제거

--- src/components/BookSections/SelectionBook/types.ts
+++ src/components/BookSections/SelectionBook/types.ts
@@ -1,18 +1,10 @@
-import { DisplayType, AIRecommendationBook, BookItem } from 'src/types/sections';
+import { DisplayType, BookItem } from 'src/types/sections';
 
 export interface SelectionBookCommonProps {
+  type: DisplayType;
   genre: string;
   slug: string;
-}
-
-export interface SelectionBookMdProps {
-  type: Exclude<DisplayType, 'AiRecommendation'>;
   items: BookItem[]; // Fixme Md 타입 말고 comics UserPreferredSection 타입이 API 결과로 오는데 이 부분 확인해야 함
 }
 
-export interface SelectionBookAiRecommendationProps {
-  type: 'AiRecommendation';
-  items: AIRecommendationBook[];
-}
-
-export type SelectionBookListProps = SelectionBookCommonProps & (SelectionBookMdProps | SelectionBookAiRecommendationProps);
+export type SelectionBookListProps = SelectionBookCommonProps;
